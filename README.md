2e23beb6e9f94ad5cc78d5f25c7f68fcadb3a3cb
Rodrigoo0m <129864834+Rodrigoo0m@users.noreply.github.com>
2024-03-17 13:47:32 +0000

    Add files via upload

---
+++ README.md
@@ -0,0 +1,30 @@
+# ISCI-DoS-Tools
+<img src="https://github.com/RCode777/ISCI-DoS-Tools/blob/main/assets/isci07.png?raw=true" width="100px">
+GUI proram DoS tools with python script <br>
+DoS attack with TCP Flooding method.
+
+# How to install?
+
+`$ git clone https://github.com/RCode777/ISCI-DoS-Tools`
+<br>
+`$ cd ISCI-DoS-Tools`
+<br>
+`$ pip install -r requirements.txt`
+<br>
+`$ python3 gui.py`
+
+### Operating system tested
+
+<ul>
+  <li>
+    Kali Linux 2022
+  </li>
+  <li>
+    Windows 10
+  </li>
+<ul>
+
+# Screenshot
+![Screenshot (33)](https://user-images.githubusercontent.com/92700881/199250095-4ef39427-dc19-47b7-af0c-3a64797c8de7.png)
+# Result
+![Screenshot (34)](https://user-images.githubusercontent.com/92700881/199251879-ab45024c-d67e-4998-b57d-851cd221483e.png)
---
+++ gui.py
@@ -0,0 +1,42 @@
+from PIL import ImageTk, Image
+from lib.function import *
+import customtkinter
+import tkinter
+
+customtkinter.set_appearance_mode("System")
+customtkinter.set_default_color_theme("dark-blue")
+
+app = customtkinter.CTk()
+app.title("~ ISCI DoS Tools ~")
+center(app, 400, 600)
+
+frame = customtkinter.CTkFrame(master=app, width=400, height=600)
+frame.pack(padx=10, pady=20, fill="x", expand=True)
+
+images = Image.open("./assets/isci07.png")
+resized_image = images.resize((100, 100))
+img = ImageTk.PhotoImage(resized_image)
+label1 = tkinter.Label(image=img)
+label1.image = img
+label1.pack()
+label1.place(x=150, y=5)
+
+TARGET = tkinter.StringVar()
+add_input(customtkinter, frame, "Target", TARGET)
+
+add_button(customtkinter, frame, "Lock Target",
+           button_event=lambda: event_button_target(customtkinter, app, TARGET.get()))
+
+PACKETS = tkinter.IntVar()
+add_input(customtkinter, frame, "Packets", PACKETS)
+
+THREADS = tkinter.IntVar()
+add_input(customtkinter, frame, "Thread", THREADS)
+
+add_button(customtkinter, frame, "Start Attack!", button_event=lambda: attack_thread(
+    TARGET.get(), PACKETS.get(), THREADS.get()))
+
+add_button(customtkinter, frame, "Stop Attack!",
+           button_event=lambda: stop(app))
+
+app.mainloop()
